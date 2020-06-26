4bfb9045c7b60f9b946bb23cfb7af4a7b5a999a4
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2020-06-26 23:55:08 +0300

    Spaceman game
    Spaceman

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Spaceman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+            g.GetName();
+            g.Greet();
+
+            Console.WriteLine("Pushed!");
+        }
+    }
+}
