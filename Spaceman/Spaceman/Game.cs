84a1d88ff1c2876384c00c14f2676438ef41b603
Andreas Georgiou <ageorgiou4@uclan.ac.uk>
2020-06-27 00:20:34 +0300

    Add files via upload

---
+++ Spaceman/Spaceman/Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Spaceman
+{
+    class Game
+    {
+        // FIELDS
+        private string player;
+        private string codeWord;
+        private string currentWord;
+        private int numberOfGuesses;
+        private int currentNumberOfWrongGuesses;
+        private string[] options;
+        private Ufo u = new Ufo();
+
+        // CONSTRUCTORS
+        public Game(string codeWord = "secret", string currentWord = "hello", int numberOfGuesses = 5, int currentNumberOfWrongGuesses = 0)
+        {
+            string[] options = new string[] { "abcdef", "ghijk" };
+            codeWord = options[1];
+            numberOfGuesses = 5;
+            currentNumberOfWrongGuesses = 0;
+            currentWord = "_____";
+        }
+
+        // PROPERTIES
+
+        // METHODS
+        public void GetName()
+        {
+            Console.WriteLine("What's your name?");
+        }
+        public void Greet()
+        {
+            player = Console.ReadLine();
+            Console.WriteLine($"Hello {player}!");
+        }
+
+        public Boolean DidWin()
+        {
+            if (codeWord.Equals(currentWord))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Boolean DidLose()
+        {
+            if (currentNumberOfWrongGuesses > numberOfGuesses)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(u.Stringify());
+            Console.WriteLine(currentWord);
+            Console.WriteLine(numberOfGuesses - currentNumberOfWrongGuesses);
+        }
+
+        public void Ask()
+        {
+            Console.WriteLine("Guess a letter");
+            string letter = Console.ReadLine();
+
+            if (letter.Length != 1)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Spaceman/Spaceman/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Spaceman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+            g.GetName();
+            g.Greet();
+        }
+    }
+}
